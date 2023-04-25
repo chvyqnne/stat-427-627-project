64ee3924095efb8a4a321c29d85ba2b2a149f055
Lexie Tyson <lexietyson@gmail.com>
2023-04-25 01:36:56 -0400

    data edition added to ridge/lasso

--- modeling/RidgeLassoPCA.R
+++ modeling/RidgeLassoPCA.R
@@ -97,3 +97,93 @@ screeplot(pc2)
 pcreg2 <- pcr(expenditures ~ ., data = dataV2, validation = "CV")
 summary(pcreg2)
 ## MSE: 5.464 x 10^6
+
+
+### New Version of Data
+#
+# I tried doing a version of the data focusing on the change in expenditures
+# as well as the change in the index measures
+
+### Loading and Filtering Data
+data <- read_csv("../data/data2.csv")
+
+data <- select(data, region, country, income_group, year, mobility, 
+               workplace, pay, marriage, parenthood, entrepreneurship, 
+               assets, expenditures)
+data <- filter(data, year > 2000, !is.na(expenditures))
+data <- pivot_wider(data, id_cols = c(country, region, income_group), 
+                    names_from = year, 
+                    values_from = mobility:expenditures)
+
+data <- mutate(data, expenditures_old = case_when(
+  !is.na(expenditures_2001) ~ expenditures_2001,
+  !is.na(expenditures_2002) ~ expenditures_2002, 
+  !is.na(expenditures_2003) ~ expenditures_2003, 
+  !is.na(expenditures_2004) ~ expenditures_2004, 
+  !is.na(expenditures_2005) ~ expenditures_2005
+)
+)
+
+data <- mutate(data, expenditures_new = case_when(
+  !is.na(expenditures_2019) ~ expenditures_2019,
+  !is.na(expenditures_2018) ~ expenditures_2018, 
+  !is.na(expenditures_2017) ~ expenditures_2017, 
+  !is.na(expenditures_2016) ~ expenditures_2016, 
+  !is.na(expenditures_2015) ~ expenditures_2015,
+  !is.na(expenditures_2014) ~ expenditures_2014,
+  !is.na(expenditures_2013) ~ expenditures_2013,
+  !is.na(expenditures_2012) ~ expenditures_2012,
+)
+)
+
+data <- select(data, region, country, income_group, mobility_2001, mobility_2019,
+               workplace_2001, workplace_2019, pay_2001, pay_2019, marriage_2001,
+               marriage_2019, parenthood_2001, parenthood_2019, entrepreneurship_2001,
+               entrepreneurship_2019, assets_2001, assets_2019, expenditures_old,
+               expenditures_new)
+data <- drop_na(data)
+
+
+data <- mutate(data, mobility = mobility_2019 - mobility_2001,
+               workplace = workplace_2019 - workplace_2001,
+               pay = pay_2019 - pay_2001,
+               marriage = marriage_2019 - marriage_2001,
+               parenthood = parenthood_2019 - parenthood_2001,
+               entrepreneurship = entrepreneurship_2019 - entrepreneurship_2001,
+               assets = assets_2019 - assets_2001,
+               expenditures = expenditures_new - expenditures_old)
+
+data <- select(data, region, country, income_group, mobility, 
+               workplace, pay, marriage, parenthood, entrepreneurship, assets,
+               expenditures)
+
+## Ridge Regression
+reg <- lm(expenditures ~ ., data = data)
+x3 <- model.matrix(reg)
+x3 <- x3[, -1]
+y3 <- data$expenditures
+
+ridge3 <- glmnet(x3, y3, alpha = 0)
+plot(ridge3, label = TRUE, xvar = "lambda")
+
+set.seed(123)
+ridge3CV <- cv.glmnet(x3, y3, alpha = 0)
+plot(ridge3CV)
+coef(ridge3CV) |> round(2)
+ridge3CV
+
+## Lasso Regression
+lasso3 <- glmnet(x3, y3)
+plot(lasso3, label = TRUE, xvar = "lambda")
+
+set.seed(123)
+lasso3CV <- cv.glmnet(x3, y3)
+plot(lasso3CV)
+coef(lasso3CV) |> round(2)
+lasso3CV
+
+## PCA Regression
+pc3 <- prcomp(x3)
+screeplot(pc3)
+pcreg3 <- pcr(expenditures ~ ., data = data, validation = "CV")
+summary(pcreg3)
